bb18024fdc142e7aceb44bfe909e791df991ce97
s3713026 <phucuong200297@email.com>
2023-08-22 17:19:37 +0700

    test

--- src/plugins/custom-app/server/routes/content-api.js
+++ src/plugins/custom-app/server/routes/content-api.js
@@ -3,15 +3,15 @@
 const config = require("@strapi/strapi/lib/core/registries/config");
 
 module.exports = {
-    type: 'content-api',
-    routes: [
-        {
-            method: "GET",
-            path:"/customapi",
-            handler:"custom-app.customAPI",
-            config:{
-                policies:[]
-            }
-        }
-    ]
+    // type: 'content-api',
+    // routes: [
+    //     {
+    //         method: "GET",
+    //         path:"/customapi",
+    //         handler:"custom-app.customAPI",
+    //         config:{
+    //             policies:[]
+    //         }
+    //     }
+    // ]
 }
