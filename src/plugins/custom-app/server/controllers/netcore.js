8ea0937ba150020dc6fabe7b4cb9aead8884da2d
s3713026 <phucuong200297@email.com>
2023-08-26 22:52:09 +0700

    3rd

--- src/plugins/custom-app/server/controllers/netcore.js
+++ src/plugins/custom-app/server/controllers/netcore.js
@@ -12,53 +12,14 @@ var netcoreQueue = Queue('netcore_queue', { redis: { port: port, host: host, pas
 module.exports ={
     async customapi(ctx) {
         ctx.body = "AKADIGITAL";
-    
-    
-      //   //Logging
-      //   let request_urls = ctx.request.url;
-      //   let request_method = ctx.request.method;
-      //   let request_record = "Lmaoez@gmail.com";
-      //   console.log("URL", request_urls);
-      //   console.log("METHOD", request_method);
-      //   console.log("Header", ctx.request.header);
-    
-    
-      //   ctx.body = ctx.request.header;
-    
-    
-      //   try {
-      //     if (request_record != "" && ctx.response.status == 200) {
-      //       let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-      //         data: {
-      //           status: ctx.response.status + " - POST SUCCESS",
-      //           method: request_method,
-      //           url: request_urls
-      //         }
-      //       });
-      //     } else {
-      //       let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-      //         data: {
-      //           status: ctx.response.status + " - POST FAIL",
-      //           method: request_method,
-      //           url: request_urls
-      //         }
-      //       });
-      //     }
-      //   } catch (error) {
-      //     let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-      //       data: {
-      //         status: ctx.response.status + " - POST FAIL",
-      //         method: request_method,
-      //         url: request_urls
-      //       }
-      //     });
-      //   }
-    
-    
-      //   let statusCode = ctx.response.status;
-      //   console.log(statusCode);
-      //   console.log("typeof statusCode:", typeof statusCode);
-      }
-    
-          
+        let loggin = await strapi.db.query('plugin::custom-app.netcorelead').create({
+          data:{
+            status: "- POST SUCCESS",
+            method: "OK",
+            url: "google.com.vn"
+
+          }
+        })
+        loggin();
+      }   
 }
