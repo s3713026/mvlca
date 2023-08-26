87fbab53605570a38b567b81c047bf28b2cc8fc9
s3713026 <phucuong200297@email.com>
2023-08-26 21:37:51 +0700

    3rd

--- src/plugins/custom-app/server/routes/index.js
+++ src/plugins/custom-app/server/routes/index.js
@@ -1,27 +1,28 @@
-// module.exports = [
-//   {
-//     method: 'GET',
-//     path: '/',
-//     handler: 'myController.index',
-//     config: {
-//       policies: [],
-//     },
-//   },
-  
-// ];
-// "use strict";
-
-// module.exports = {
-//   "content-api": require('./content-api')
-// };
 module.exports = [
   {
     method: 'GET',
     path: '/',
-    handler: 'Netcore.customapi',
+    handler: 'myController.index',
     config: {
       policies: [],
     },
   },
   
 ];
+// "use strict";
+
+// module.exports = {
+//   "content-api": require('./content-api')
+// };
+// module.exports = [
+//   {
+//     method: 'GET',
+//     path: '/',
+//     handler: 'Netcore.customapi',
+//     config: {
+//       policies: [],
+//     },
+//   },
+  
+// ];
+
