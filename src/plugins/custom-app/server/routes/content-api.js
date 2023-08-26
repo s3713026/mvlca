9d5d77f1d731e5254aca1f5b47ecf2f7a255ef52
s3713026 <phucuong200297@email.com>
2023-08-26 23:44:24 +0700

    3rd

--- src/plugins/custom-app/server/routes/content-api.js
+++ src/plugins/custom-app/server/routes/content-api.js
@@ -5,10 +5,18 @@ const config = require("@strapi/strapi/lib/core/registries/config");
 module.exports = {
     type: 'content-api',
     routes: [
+        // {
+        //     method: "GET",
+        //     path:"/customapi",
+        //     handler:"Netcore.customapi",
+        //     config:{
+        //         policies:[]
+        //     }
+        // },
         {
-            method: "GET",
-            path:"/customapi",
-            handler:"Netcore.customapi",
+            method: "POST",
+            path:"/webhook/send",
+            handler:"Netcore.send",
             config:{
                 policies:[]
             }
