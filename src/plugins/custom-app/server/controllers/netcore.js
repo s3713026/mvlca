01e5a6dd616f7ca813d7e9fe8ef8d62ce026ba95
s3713026 <phucuong200297@email.com>
2023-09-04 10:52:38 +0700

    5

--- src/plugins/custom-app/server/controllers/netcore.js
+++ src/plugins/custom-app/server/controllers/netcore.js
@@ -11,146 +11,168 @@ let password = strapi.config.get('server.redis.password', '');
 
 var netcoreQueue = Queue('netcore_queue', { redis: { port: port, host: host, password: password } });
 
-module.exports ={
-    // async customapi(ctx) {
-    //     console.log(ctx.request.body);
-    //     console.log(ctx.request.header);
-    //     ctx.body = "AKADIGITAL";
-      
-    
-    //     //Logging
-    //     let request_urls = "Truong phu cuong";
-    //     let request_method = "0349305195";
-    //     let request_record = "phucuong200297@gmail.com";
-    //     console.log("URL", request_urls);
-    //     console.log("METHOD", request_method);
-    //     console.log("Header", ctx.request.header);
-    
-    
-    //     ctx.body = ctx.request.header;
-    
-    
-    //     try {
-    //       if (request_record != "" && ctx.response.status == 200) {
-    //         let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-    //           data: {
-    //             Email: request_record,
-    //             Mobile: request_method,
-    //             Full_Name: request_urls
-    //           }
-    //         });
-    //       } else {
-    //         let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-    //           data: {
-    //             Email: request_record,
-    //             Mobile: request_method,
-    //             Full_Name: request_urls
-    //           }
-    //         });
-    //       }
-    //     } catch (error) {
-    //       let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
-    //         data: {
-    //           Email: request_record,
-    //           Mobile: request_method,
-    //           Full_Name: request_urls
-    //         }
-    //       });
-    //     }
-    
-    
-    //     let statusCode = ctx.response.status;
-    //     console.log(statusCode);
-    //     console.log("typeof statusCode:", typeof statusCode);
-        
-    //   }
+module.exports = {
+  // async customapi(ctx) {
+  //     console.log(ctx.request.body);
+  //     console.log(ctx.request.header);
+  //     ctx.body = "AKADIGITAL";
 
-    async send(ctx){
-      ctx.body = "AKADIGITAL"
-      var basic_auth_panel = Buffer.from(ctx.request.header.authorization.split(" ")[1],'base64').toString();
-      var arr = basic_auth_panel.split(":");
-      const basic_auth = await strapi.db.query('plugin::users-permissions.user').findOne({
-        where: {
-          username: 'akadigital'
+
+  //     //Logging
+  //     let request_urls = "Truong phu cuong";
+  //     let request_method = "0349305195";
+  //     let request_record = "phucuong200297@gmail.com";
+  //     console.log("URL", request_urls);
+  //     console.log("METHOD", request_method);
+  //     console.log("Header", ctx.request.header);
+
+
+  //     ctx.body = ctx.request.header;
+
+
+  //     try {
+  //       if (request_record != "" && ctx.response.status == 200) {
+  //         let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
+  //           data: {
+  //             Email: request_record,
+  //             Mobile: request_method,
+  //             Full_Name: request_urls
+  //           }
+  //         });
+  //       } else {
+  //         let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
+  //           data: {
+  //             Email: request_record,
+  //             Mobile: request_method,
+  //             Full_Name: request_urls
+  //           }
+  //         });
+  //       }
+  //     } catch (error) {
+  //       let logging = await strapi.db.query('plugin::custom-app.netcorelead').create({
+  //         data: {
+  //           Email: request_record,
+  //           Mobile: request_method,
+  //           Full_Name: request_urls
+  //         }
+  //       });
+  //     }
+
+
+  //     let statusCode = ctx.response.status;
+  //     console.log(statusCode);
+  //     console.log("typeof statusCode:", typeof statusCode);
+
+  //   }
+
+  async send(ctx) {
+    ctx.body = "AKADIGITAL"
+    var basic_auth_panel = Buffer.from(ctx.request.header.authorization.split(" ")[1], 'base64').toString();
+    var arr = basic_auth_panel.split(":");
+    const basic_auth = await strapi.db.query('plugin::users-permissions.user').findOne({
+      where: {
+        username: 'akadigital'
+      }
+    });
+
+
+    var check = await bcrypt.compare(arr[1], basic_auth.password)
+    if (check == true) {
+      ctx.body = "Welcome webhook"
+
+      let request_urls = ctx.request.url;
+      let request_method = ctx.request.method;
+      let request_record = ctx.request["x-forwarded-for"];
+
+      let data = ctx.request.body.data;
+      console.log('Raw data here', data)
+      let data_body = JSON.parse(data);
+      console.log("Data in here:", data_body);
+      let Email = '';
+      let Mobile = '';
+      let Agent_Email = '';
+      let Agent_Id = '';
+
+      Email = data_body.voolatechsmt[0].att_params.EMAIL;
+      Mobile = data_body.voolatechsmt[0].att_params.MOBILE;
+      Agent_Email = data_body.voolatechsmt[0].att_params.AGENT_MAIL;
+      Agent_Id = data_body.voolatechsmt[0].att_params.AGENT_ID;
+
+      let data_list = [];
+
+      data_list.push({
+        'customer': Email,
+        'customer_mobile': Mobile,
+        'agent': Agent_Email
+      })
+      console.log(data_list);
+
+      const getAgent = await strapi.db.query('plugin::custom-app.agent').findMany({
+        select: ['email', 'id'],
+      })
+      let i = 0;
+      for (i = 0; i <= getAgent.length; i++) {
+        if (getAgent[i].Email == Agent_Email) {
+          const updateAgent = await strapi.db.query('api::blog.article').update({
+            where: { id: getAgent[i].id },
+            data: {
+              'netcorelead': {
+                'Email': Email,
+                'Mobile': Mobile
+              }
+            },
+          });
+        } else {
+          let entry2 = await strapi.db.query('plugin::custom-app.agent').create({
+            data: {
+              'Email': Agent_Email,
+              'netcorelead': {
+                'Email': Email,
+                'Mobile': Mobile
+              }
+            }
+          });
         }
-      });
-  
-  
-      var check = await bcrypt.compare(arr[1], basic_auth.password)
-      if (check == true){
-        ctx.body = "Welcome webhook"
+      }
 
-        let request_urls = ctx.request.url;
-        let request_method = ctx.request.method;
-        let request_record = ctx.request["x-forwarded-for"];
 
-        let data = ctx.request.body.data;
-        console.log('Raw data here',data)
-        let data_body = JSON.parse(data);
-        console.log("Data in here:",data_body);
-        let Email ='';
-        let Mobile ='';
-        let Agent_Email='';
-        let Agent_Id ='';
-
-        Email = data_body.voolatechsmt[0].att_params.EMAIL;
-        Mobile = data_body.voolatechsmt[0].att_params.MOBILE;
-        Agent_Email = data_body.voolatechsmt[0].att_params.AGENT_MAIL;
-        Agent_Id = data_body.voolatechsmt[0].att_params.AGENT_ID;
-        
-        let data_list =[];
-
-        data_list.push({
-          'customer': Email,
-          'customer_mobile':Mobile,
-          'agent': Agent_Email
-        })
-        console.log(data_list);
-
-        const getAgent = await strapi.db.query('plugin::custom-app.agent').findMany({
-          select:['email'],
-        })
-        console.log(getAgent);
-        
-
-        let entry = await strapi.db.query('plugin::custom-app.netcorelead').create({
-          data: {
-            'Email': Email,
-            'Mobile':Mobile,
-          }
-        });    
-        let entry2 = await strapi.db.query('plugin::custom-app.agent').create({
-          data: {
+      let entry = await strapi.db.query('plugin::custom-app.netcorelead').create({
+        data: {
+          'Email': Email,
+          'Mobile': Mobile,
+          'agent': {
             'Email': Agent_Email,
           }
-        }); 
-      }
-    },
-
-    async get_cus_for_agent(ctx){
-      console.log(ctx.request.body);
-      console.log(ctx.request.header);
-
-      const getcustomer = await strapi.db.query('plugin::custom-app.agent').findMany({
-        select:['email'],
-        populate:{netcorelead:true}
-      })
-      console.log(getcustomer)
-      ctx.body = getcustomer;
-    },
-    
-    async send_cus(ctx){
-    
-      const customer= await strapi.db.query('plugin::custom-app.netcorelead').findMany({
-        select: ['email','mobile'],
-        populate:{agent:true}
+        }
       });
-      console.log(customer);
 
-      // console.log(count);
-      // ctx.body = count;
     }
-          
+  },
+
+  async get_cus_for_agent(ctx) {
+    console.log(ctx.request.body);
+    console.log(ctx.request.header);
+
+    const getcustomer = await strapi.db.query('plugin::custom-app.agent').findMany({
+      select: ['email'],
+      populate: { netcorelead: true }
+    })
+    console.log(getcustomer)
+    ctx.body = getcustomer;
+  },
+
+  async send_cus(ctx) {
+
+    const customer = await strapi.db.query('plugin::custom-app.netcorelead').findMany({
+      select: ['email', 'mobile'],
+      populate: { agent: true }
+    });
+    console.log(customer);
+
+    // console.log(count);
+    // ctx.body = count;
+  }
+
 }
 // smartech('identify','');
 // smartech('contact', 'LIST IDENTIFIER', {
