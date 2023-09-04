90ca53c4cd8ec5a2149fbcb6fa05721e264d0fb8
s3713026 <phucuong200297@email.com>
2023-09-04 11:29:02 +0700

    5

--- src/plugins/custom-app/server/controllers/netcore.js
+++ src/plugins/custom-app/server/controllers/netcore.js
@@ -110,7 +110,8 @@ module.exports = {
       const getAgent = await strapi.db.query('plugin::custom-app.agent').findMany({
         select: ['email', 'id'],
       })
-      console.log(getAgent[0].email);
+      let data_agent = JSON.parse(getAgent);
+      console.log(data_agent);
       let i = 0;
       for (i = 0; i <= getAgent.length; i++) {
         if (getAgent[i].Email == Agent_Email) {
