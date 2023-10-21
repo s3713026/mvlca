3c3faa782efe98e018ae83afab7a9105d955f8a2
s3713026 <phucuong200297@email.com>
2023-10-21 18:06:38 +0700

    ok

--- src/plugins/custom-app/server/controllers/netcore.js
+++ src/plugins/custom-app/server/controllers/netcore.js
@@ -191,7 +191,7 @@ module.exports = {
   async send_cus(ctx) {
 
     const customer = await strapi.db.query('plugin::custom-app.netcorelead').findMany({
-      select: ['email','MOBILE','AGENT'],
+      select: ['Email','Mobile','Agent'],
       populate: { agent: true }
     });
     console.log(customer);
